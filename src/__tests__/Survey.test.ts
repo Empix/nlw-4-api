29fca77a5ba23f389305616c85efce6d83931cc7
Empix <jotinhave@gmail.com>
2021-02-26 02:58:22 -0400

    Create tests with jest

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,27 @@
+import request from 'supertest';
+
+import { app } from '../app';
+
+import createConnection from '../database';
+
+describe('Survey', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it('Should be able to create a new survey', async () => {
+    const response = await request(app).post('/surveys').send({
+      title: 'Test',
+      description: 'Test description',
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('Should be able to get all surveys', async () => {
+    const response = await request(app).get('/surveys');
+
+    expect(response.body.length).toBe(1);
+  });
+});
